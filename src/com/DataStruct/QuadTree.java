b042276dad1fa5e8a16a442dec9ae6da4872d7a5
NicholasZanetiAulivier <nicholas.535240054@stu.untar.ac.id>
2025-05-21 10:43:30 +0700

    More progress pm QuadTree

--- src/com/DataStruct/QuadTree.java
+++ src/com/DataStruct/QuadTree.java
@@ -21,6 +21,27 @@ public class QuadTree<T> {
     public QuadTree(byte l , int x , int y , int width , int height ){
         this(l,new Rectangle(x, y, width, height));
     }
+
+    public void split(){
+        int childrenWidth = (int)(bounds.getWidth()/2);
+        int childrenHeight = (int)(bounds.getHeight()/2);
+        int x = (int)bounds.getX();
+        int y = (int)bounds.getY();
+
+        childrenNodes = new QuadTree[4];
+        byte childrenLevel = (byte)(level+1);
+
+        //Top left
+        childrenNodes[0] = new QuadTree(childrenLevel, x, y, childrenWidth, childrenHeight);
+        //Top right
+        childrenNodes[1] = new QuadTree(childrenLevel, x+childrenWidth, y, childrenWidth, childrenHeight);
+        //Bot left
+        childrenNodes[2] = new QuadTree(childrenLevel, x, y+childrenHeight, childrenWidth, childrenHeight);
+        //Bot right
+        childrenNodes[3] = new QuadTree(childrenLevel, x+childrenWidth, y+childrenHeight, childrenWidth, childrenHeight);
+    }
+
+
     
 
 }
