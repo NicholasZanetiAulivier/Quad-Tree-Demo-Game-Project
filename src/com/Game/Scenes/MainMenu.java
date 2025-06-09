11e58991e00395fe338bed3730547a549bd9fdb8
NicholasZanetiAulivier <nicholas.535240054@stu.untar.ac.id>
2025-06-09 13:44:13 +0700

    Cleanup

--- src/com/Game/Scenes/MainMenu.java
+++ src/com/Game/Scenes/MainMenu.java
@@ -1,23 +1,21 @@
 package com.Game.Scenes;
 
-import com.Game.Objects.CreditsButton;
-import com.Game.Objects.PlayButton;
-
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-
 import javax.imageio.ImageIO;
 
+import com.Game.Objects.PlayButton;
 import com.Game.Engine.Global;
 
 public class MainMenu extends Scene {
     public PlayButton playButton;
-    public CreditsButton creditsButton;
     public BufferedImage background;
 
     @SuppressWarnings("all")
     public void loadScene() throws Exception{
         playButton = new PlayButton((Global.realWidth-playButton.WIDTH)*.5f, 500);
-        creditsButton = new CreditsButton((Global.realWidth-creditsButton.WIDTH)*.5f, 400);
         background = ImageIO.read(MainMenu.class.getResource("background/cloudBackground.jpg"));
 
         Global.GAME_ENVIRONMENT.setUpdateFunction(
@@ -29,12 +27,6 @@ public class MainMenu extends Scene {
                     && mouseY > playButton.position.y
                     && mouseY < playButton.position.y+playButton.HEIGHT
                 ) playButton.hovering = true;
-                
-                if(mouseX > creditsButton.position.x
-                    && mouseX < creditsButton.position.x+creditsButton.WIDTH
-                    && mouseY > creditsButton.position.y
-                    && mouseY < creditsButton.position.y+creditsButton.HEIGHT
-                ) creditsButton.hovering = true;
 
             }
         );
@@ -53,17 +45,6 @@ public class MainMenu extends Scene {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
-                
-                if(mouseX > creditsButton.position.x
-                    && mouseX < creditsButton.position.x+creditsButton.WIDTH
-                    && mouseY > creditsButton.position.y
-                    && mouseY < creditsButton.position.y+creditsButton.HEIGHT
-                ) try {
-                    Global.Visualizer.switchScene();
-                } catch(Exception e){
-                    e.printStackTrace();
-                }
-
             }
         );
 
@@ -75,17 +56,25 @@ public class MainMenu extends Scene {
 
         Global.CANVAS[0].setDrawFunction(
             (g) -> {
-                creditsButton.draw(g , Global.CANVAS[0]);
                 playButton.draw(g , Global.CANVAS[0]);
                 playButton.hovering = false;
-                creditsButton.hovering = false;
+
+                g.setColor(new Color(.5f,.5f,.5f,.5f));
+                ((Graphics2D)g).fillRect(100, 150, 600, 300);
+                g.setColor(Color.BLACK);
+                ((Graphics2D)g).drawRect(100, 150, 600, 300);
+                ((Graphics2D)g).setFont(new Font("Arial", Font.BOLD, 30));
+                ((Graphics2D)g).drawString("'r' - Restart", 150, 200);
+                ((Graphics2D)g).drawString("'z' - Shoot", 150, 250);
+                ((Graphics2D)g).drawString("'Shift' - Focus", 150, 300);
+                ((Graphics2D)g).drawString("'Esc/p' - Pause", 150, 350);
+                ((Graphics2D)g).drawString("'F3' - Debug", 150, 400);
             }
         );
 
 
         //Load classes
         PlayButton.loadSprite();
-        CreditsButton.loadSprite();
     }
 
     @SuppressWarnings("all")
@@ -97,10 +86,8 @@ public class MainMenu extends Scene {
         
         //unload classes
         PlayButton.unload();
-        CreditsButton.unload();
 
         //Unload objects
         playButton = null;
-        creditsButton = null;
     }
 }
